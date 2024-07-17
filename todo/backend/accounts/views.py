3e396ff5b162bed08e2a0eb58ccf12fcd9286638
Hitomi Uchihi <pecobitaro@gmail.com>
2024-07-17 18:37:36 +0900

    ログイン後の正常なルーティングを確認

--- todo/backend/accounts/views.py
+++ todo/backend/accounts/views.py
@@ -13,7 +13,7 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('some_view_name')  # ログイン後のリダイレクト先
+                return redirect('http://localhost:3003')  # ログイン後のリダイレクト先
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
